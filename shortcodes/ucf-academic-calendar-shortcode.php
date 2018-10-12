fb2617b01657f1606cb11496dfe27b3027c64b41
Jim Barnes <jimbarnesdeveloper@gmail.com>
2018-10-12 09:48:36 -0400

    Updated feed to use get_shortcode_defaults

--- shortcodes/ucf-academic-calendar-shortcode.php
+++ shortcodes/ucf-academic-calendar-shortcode.php
@@ -14,6 +14,8 @@ if ( ! class_exists( 'UCF_Acad_Cal_Shortcode' ) ) {
 		public static function handler( $atts, $content='' ) {
 			$atts = shortcode_atts( UCF_Acad_Cal_Config::get_shortcode_defaults(), $atts );
 
+			var_dump( $atts );
+
 			$items = UCF_Acad_Cal_Feed::get_feed( $atts );
 
 			ob_start();
